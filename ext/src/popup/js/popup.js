dcb1882bf0d9cfa6cb3aaa3ae830847de54109fe
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-07-18 18:36:38 +0530

    added remove task

--- ext/src/popup/js/popup.js
+++ ext/src/popup/js/popup.js
@@ -1,77 +1,96 @@
 //send mgs to background
 
 $("#addTaskBtn").click(() => {
-  let taskName = $("#taskNameInput").val();
-  console.log("add task clicked", taskName);
-  chrome.runtime.sendMessage(
-    {
-      action: "ADD_TASK",
-      payload: {
-        taskName: taskName,
-      },
-    },
-    (response) => {
-      console.log("count", response);
-      refreshTasks();
-      $("#taskNameInput").val("");
-    }
-  );
+    let taskName = $("#taskNameInput").val();
+    console.log("add task clicked", taskName);
+    chrome.runtime.sendMessage(
+        {
+            action: "ADD_TASK",
+            payload: {
+                taskName: taskName,
+            },
+        },
+        (response) => {
+            console.log("count", response);
+            refreshTasks();
+            $("#taskNameInput").val("");
+        }
+    );
+});
+
+
+$(document).on("click", ".delBtn", function () {
+    console.log("remove task", this.parentElement.id);
+    chrome.runtime.sendMessage(
+        {
+            action: "REMOVE_TASK",
+            payload: {
+                taskId: this.parentElement.id,
+            },
+        },
+        (tasks) => {
+            console.log("response", tasks);
+            refreshTasks();
+        }
+    );
 });
 
 $(document).on("change", ".status_checkbox", function () {
-  console.log("status changed", this.id);
-  chrome.runtime.sendMessage(
-    {
-      action: "TOGGLE_STATUS",
-      payload: {
-        taskId: this.id,
-      },
-    },
-    (tasks) => {
-      console.log("response", tasks);
-      refreshTasks();
-    }
-  );
+    console.log("status changed", this.parentElement.id);
+    chrome.runtime.sendMessage(
+        {
+            action: "TOGGLE_STATUS",
+            payload: {
+                taskId: this.parentElement.id,
+            },
+        },
+        (tasks) => {
+            console.log("response", tasks);
+            refreshTasks();
+        }
+    );
 });
 
 function refreshTasks() {
-  chrome.runtime.sendMessage(
-    {
-      action: "GET_TASKS",
-    },
-    (tasks) => {
-      console.log("response", tasks);
-      updateToTaskList(tasks);
-    }
-  );
+    chrome.runtime.sendMessage(
+        {
+            action: "GET_TASKS",
+        },
+        (tasks) => {
+            console.log("response", tasks);
+            updateToTaskList(tasks);
+        }
+    );
 }
 
 function updateToTaskList(tasks) {
-  $("#taskList").html("");
-  $("#completedTaskList").html("");
+    $("#taskList").html("");
+    $("#completedTaskList").html("");
 
-  const todoTasks = tasks.filter((task) => task.isCompleted === false);
-  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+    const todoTasks = tasks.filter((task) => task.isCompleted === false);
+    const completedTasks = tasks.filter((task) => task.isCompleted === true);
 
-  for (const task of todoTasks) {
-    $("#taskList").append(`<li class="list-group-item">
-    <input class="status_checkbox" id="${task.id}" type="checkbox" 
+    for (const task of todoTasks) {
+        $("#taskList").append(`<li id="${task.id}" class="list-group-item">
+    <input class="status_checkbox" id="chk_${task.id}" type="checkbox" 
     >
-    <label for="${task.id}">${task.name}</label>
+    <label for="chk_${task.id}">${task.name}</label>
+    <button class="delBtn">remove</button>
     </li>`);
-  }
-  for (const task of completedTasks) {
-    $("#completedTaskList").append(`<li class="list-group-item">
-    <input class="status_checkbox" id="${task.id}" type="checkbox" 
+    }
+    for (const task of completedTasks) {
+        $("#completedTaskList").append(`<li id="${task.id}" class="list-group-item">
+    <input class="status_checkbox" id="chk_${task.id}" type="checkbox" 
     checked=true
     >
-    <label for="${task.id}">${task.name}</label>
+    <label for="chk_${task.id}">${task.name}</label>
+    <button class="delBtn">remove</button>
     </li>`);
-  }
+    }
 }
 
 function init() {
-  refreshTasks();
+    refreshTasks();
 }
 
 init();
