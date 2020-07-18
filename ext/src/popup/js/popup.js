e034f0f3f93bbf0ac0aa2f5af9a763b3a5e05d0a
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-07-18 15:47:17 +0530

    functionality to maintain task

--- ext/src/popup/js/popup.js
+++ ext/src/popup/js/popup.js
@@ -1,9 +1,77 @@
 //send mgs to background
 
-chrome.runtime.sendMessage(
-    { action: "GET_COUNT", from: "popup" },
-    response => {
-        console.log("count", response);
-        this.count = response.count;
+$("#addTaskBtn").click(() => {
+  let taskName = $("#taskNameInput").val();
+  console.log("add task clicked", taskName);
+  chrome.runtime.sendMessage(
+    {
+      action: "ADD_TASK",
+      payload: {
+        taskName: taskName,
+      },
+    },
+    (response) => {
+      console.log("count", response);
+      refreshTasks();
+      $("#taskNameInput").val("");
     }
-);
+  );
+});
+
+$(document).on("change", ".status_checkbox", function () {
+  console.log("status changed", this.id);
+  chrome.runtime.sendMessage(
+    {
+      action: "TOGGLE_STATUS",
+      payload: {
+        taskId: this.id,
+      },
+    },
+    (tasks) => {
+      console.log("response", tasks);
+      refreshTasks();
+    }
+  );
+});
+
+function refreshTasks() {
+  chrome.runtime.sendMessage(
+    {
+      action: "GET_TASKS",
+    },
+    (tasks) => {
+      console.log("response", tasks);
+      updateToTaskList(tasks);
+    }
+  );
+}
+
+function updateToTaskList(tasks) {
+  $("#taskList").html("");
+  $("#completedTaskList").html("");
+
+  const todoTasks = tasks.filter((task) => task.isCompleted === false);
+  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+
+  for (const task of todoTasks) {
+    $("#taskList").append(`<li class="list-group-item">
+    <input class="status_checkbox" id="${task.id}" type="checkbox" 
+    >
+    <label for="${task.id}">${task.name}</label>
+    </li>`);
+  }
+  for (const task of completedTasks) {
+    $("#completedTaskList").append(`<li class="list-group-item">
+    <input class="status_checkbox" id="${task.id}" type="checkbox" 
+    checked=true
+    >
+    <label for="${task.id}">${task.name}</label>
+    </li>`);
+  }
+}
+
+function init() {
+  refreshTasks();
+}
+
+init();
